57d46d035ab68cb469536032ecb1b529594f4e95
Roman Filatov <ikardoc@gmail.com>
2022-05-24 14:51:35 +0300

    Added Calc Logic and NuGet Libs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Calculator
+{
+    public int Additional(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtraction(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Miltiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Division(int a, int b)
+    {
+        return a / b;
+    }
+}
