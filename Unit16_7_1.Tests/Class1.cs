588803b03ca38db0130bd5d179c0e99359a06cba
Roman Filatov <ikardoc@gmail.com>
2022-05-24 15:23:41 +0300

    Moved Projects. Added Tests

---
+++ Unit16_7_1.Tests/Class1.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace Unit16_7_1.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestAdditional()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.True(calculator.Additional(2, 3) == 5);
+        }
+
+        [Test]
+        public void TestSubtraction()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.True(calculator.Subtraction(10, 2) == 8);
+        }
+
+        [Test]
+        public void TestMultiplication()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.True(calculator.Multiplication(4, 5) == 20);
+        }
+
+        [Test]
+        public void TestDivision()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.True(calculator.Division(20, 4) == 5);
+        }
+    }
+}
